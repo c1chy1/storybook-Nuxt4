7a7a9460ebba628a112278d5d0a11071bba17c5a
cichy <cichy08081987pp@gmail.com>
2026-02-16 17:35:54 +0100

    add EAvatars enum for avatar component types

---
+++ types/src/enums/artifact/avatars.ts
@@ -0,0 +1,10 @@
+/**
+ * @enum {string} EAvatars
+ * @description Defines types used for the avatar component.
+ * @prop AVATAR Represents an avatar with a provided image
+ * @prop INITIALS Accepts characters as an avatar instead of an image
+ */
+export enum EAvatars {
+    AVATAR = 'avatar',
+    INITIALS = 'initials',
+}
