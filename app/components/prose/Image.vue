797ca13c187127ba0145472e23d7cb5775b6b642
cichy <cichy08081987pp@gmail.com>
2026-02-19 16:41:25 +0100

    adjust Image.vue: restructure template, inherit attributes, and fix Icon import

--- app/components/prose/Image.vue
+++ app/components/prose/Image.vue
@@ -1,52 +1,56 @@
 <template>
-  <label
-      v-if="label?.length"
-      :for="id?.toString() ?? ''"
-      class="mb-1 text-tiny"
-  >
-    <span>
-      {{ label }}
-    </span>
-  </label>
-  <div
-      :class="{
-      'cursor-pointer': openInNewTab || download,
-      'relative flex items-center justify-center border-2 roundedborder-radius-lg borderadditional-grey-light overflow-hidden':
-        fullscreen,
-    }"
-      :style="
-      isExpanded
-        ? `width: 100%; height: 100%`
-        : `width: ${width ?? '200'}px; height: ${height ?? '200'}px;`
-    "
-      class="w-full h-full"
-      tabindex="0"
-      @click="handleImageClick"
-  >
-    <img
-        :id="id?.toString() ?? ''"
-        :alt="alt"
-        :aria-description="alt"
-        :aria-label="label"
-        :src="(source || blobUrl) ?? ''"
-        :style="`object-fit: ${fit ?? EFittings.COVER}; ${customStyle}`"
-        class="w-full h-full"
+  <div class="w-full">
+    <label
+        v-if="label?.length"
+        :for="id?.toString() ?? ''"
+        class="mb-1 text-tiny"
     >
+      <span>
+        {{ label }}
+      </span>
+    </label>
+
     <div
-        v-if="fullscreen"
-        class="right-1.5 bottom-1.5 absolute flex justify-center items-center bg-white shadow-shadow-even-sm rounded-border-radius-sm w-5 h-5 cursor-pointer"
-        tabindex="0"
-        @click.stop="handleResizeClick"
-        @keydown.space.prevent="handleResizeClick"
-    >
-      <Icon
-          :title="
+        :class="{
+          'cursor-pointer': openInNewTab || download,
+          'relative flex items-center justify-center border-2 roundedborder-radius-lg borderadditional-grey-light overflow-hidden':
+            fullscreen,
+        }"
+        :style="
           isExpanded
-            ? 'material-symbols:collapse-content'
-            : 'material-symbols:expand-content'
+            ? `width: 100%; height: 100%`
+            : `width: ${width ?? '200'}px; height: ${height ?? '200'}px;`
         "
-          custom-style="font-size: 0.85rem;"
-      />
+        class="w-full h-full"
+        tabindex="0"
+        v-bind="$attrs"
+        @click="handleImageClick"
+    >
+      <img
+          :id="id?.toString() ?? ''"
+          :alt="alt"
+          :aria-description="alt"
+          :aria-label="label"
+          :src="(source || blobUrl) ?? ''"
+          :style="`object-fit: ${fit ?? EFittings.COVER}; ${customStyle}`"
+          class="w-full h-full"
+      >
+      <div
+          v-if="fullscreen"
+          class="right-1.5 bottom-1.5 absolute flex justify-center items-center bg-white shadow-shadow-even-sm rounded-border-radius-sm w-5 h-5 cursor-pointer"
+          tabindex="0"
+          @click.stop="handleResizeClick"
+          @keydown.space.prevent="handleResizeClick"
+      >
+        <Icon
+            :title="
+              isExpanded
+                ? 'material-symbols:collapse-content'
+                : 'material-symbols:expand-content'
+            "
+            custom-style="font-size: 0.85rem;"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -56,7 +60,11 @@ import type {IPropsImage} from '../../../types/src'
 import {EFittings} from '../../../types/src'
 /*import { getDocument, getWindow } from '../../../types/src'*/
 import {ref, watch} from 'vue'
-import {Icon} from '../../components/artifact'
+import Icon from '../artifact/Icon.vue'
+
+defineOptions({
+  inheritAttrs: false,
+})
 
 /* Props ----------- */
 const props = withDefaults(defineProps<IPropsImage>(), {})
