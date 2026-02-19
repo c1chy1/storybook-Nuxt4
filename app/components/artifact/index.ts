3ce92817c0156fd1ff4896573770f62e87fe6372
cichy <cichy08081987pp@gmail.com>
2026-02-19 20:21:44 +0100

    add Badge and CloseButton components to artifact index barrel file

--- app/components/artifact/index.ts
+++ app/components/artifact/index.ts
@@ -1,2 +1,4 @@
 export {default as Avatar} from './Avatar.vue'
 export {default as Icon} from './Icon.vue'
+export {default as Badge} from './Badge.vue'
+export {default as CloseButton} from './CloseButton.vue'
