b7ed270a9f50ebf8a9dff6ed11f0bf890332d7e0
cichy <cichy08081987pp@gmail.com>
2026-02-19 16:41:04 +0100

    fix typo in class binding for Avatar component and add fallback rendering logic for INITIALS type

--- app/components/artifact/Avatar.vue
+++ app/components/artifact/Avatar.vue
@@ -6,7 +6,7 @@
     <div
         :aria-description="title"
         :aria-label="title"
-        :class="{ 'bgadditional-grey': type === EAvatars.INITIALS }"
+        :class="{ 'bg-additional-grey': type === EAvatars.INITIALS }"
         :style="avatar?.customStyle"
         class="h-7 w-7 rounded-border-radius-full shrink-0 flex items-center justify-center overflow-hidden"
     >
@@ -24,6 +24,7 @@
                 :type="avatar?.type"
       />
       <span
+          v-else-if="type === EAvatars.INITIALS"
           class="font-normal text-additional-white leading-none mt-[2px]"
           style="font-size: 0.8rem"
       >
@@ -39,11 +40,14 @@
 <script lang="ts" setup>
 import type {IPropsAvatar} from '../../../types/src'
 import {EAvatars} from '../../../types/src'
+import Image from '../prose/Image.vue'
 
 /* Props ----------- */
 withDefaults(defineProps<IPropsAvatar>(), {
   type: EAvatars.INITIALS,
 })
+
+console.log(EAvatars.AVATAR)
 
 /* Locals ---------- */
 /* Reactives ------- */
