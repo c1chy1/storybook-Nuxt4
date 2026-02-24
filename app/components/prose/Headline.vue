a2206f4e45d4bb0728103247ec7ac9b61cbda4c5
cichy <cichy08081987pp@gmail.com>
2026-02-24 17:19:13 +0100

    add Headline component and stories: implement new prose component with classification and format support, along with detailed Storybook examples

---
+++ app/components/prose/Headline.vue
@@ -0,0 +1,44 @@
+<template>
+  <component
+      :is="format"
+      :aria-description="title"
+      :aria-label="title"
+      :class="computedClassTag ?? HEADLINES[format]"
+  >
+    <slot>
+      {{ title }}
+    </slot>
+  </component>
+</template>
+
+<script lang="ts" setup>
+import type {IPropsHeadline} from '../../../types/src'
+import {EHeadlines} from '../../../types/src'
+import {computed} from 'vue'
+
+/* Props ----------- */
+const props = withDefaults(defineProps<IPropsHeadline>(), {
+  classification: EHeadlines.H1,
+  format: EHeadlines.H1,
+})
+
+/* Locals ---------- */
+const HEADLINES: Record<EHeadlines, string> = {
+  [EHeadlines.H1]: 'text-4xl leading-none',
+  [EHeadlines.H2]: 'text-3xl leading-none',
+  [EHeadlines.H3]: 'text-2xl leading-none',
+  [EHeadlines.H4]: 'text-xl leading-none',
+  [EHeadlines.H5]: 'text-lg leading-none',
+  [EHeadlines.H6]: 'text-base leading-none',
+}
+
+/* Reactives ------- */
+const computedClassTag = computed(() => HEADLINES[props.classification])
+
+/* Watcher --------- */
+/* Methods --------- */
+/* Emits ----------- */
+/* Lifecycle ------- */
+/* Exposings ------- */
+/* Others ---------- */
+</script>
---
+++ app/stories/prose/Headline.stories.ts
@@ -0,0 +1,225 @@
+import type {Meta, StoryFn} from '@storybook/vue3-vite'
+import {Headline} from '../../components/prose'
+import {EHeadlines} from '../../../types/src'
+
+const meta: Meta<typeof Headline> = {
+    argTypes: {
+        classification: {
+            control: {
+                type: 'select',
+            },
+            description:
+                'Defines which HTML class tag the component should be rendered in',
+            options: Object.values(EHeadlines),
+            table: {
+                defaultValue: {
+                    summary: 'EHeadlines.H1',
+                },
+                type: {
+                    summary: 'EHeadlines | undefined',
+                },
+            },
+        },
+        format: {
+            control: {
+                type: 'select',
+            },
+            description:
+                'Defines which HTML class tag the component should be shown as',
+            options: Object.values(EHeadlines),
+            table: {
+                defaultValue: {
+                    summary: 'EHeadlines.H1',
+                },
+                type: {
+                    summary: 'EHeadlines | undefined',
+                },
+            },
+        },
+        title: {
+            control: {
+                type: 'text',
+            },
+            description: 'Sets the title of the headline',
+            table: {
+                defaultValue: {
+                    summary: 'undefined',
+                },
+                type: {
+                    summary: 'string | undefined',
+                },
+            },
+        },
+    },
+    args: {},
+    component: Headline,
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    'The `<PzHeadline>` component is used to display a headline with a specific format and classification.',
+            },
+        },
+    },
+    title: 'components/Prose/Headline',
+} as Meta<typeof Headline>
+
+const Default: StoryFn<typeof Headline> = args => ({
+    components: {
+        Headline,
+    },
+    setup() {
+        return {
+            args,
+        }
+    },
+    template: `
+      <Headline v-bind="args"/>`,
+})
+Default.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story:
+                'This is the view of the headline component with H1 classification and format.',
+        },
+    },
+}
+Default.args = {
+    classification: EHeadlines.H1,
+    format: EHeadlines.H1,
+    title: 'Headline',
+}
+
+const H2 = Default.bind({})
+H2.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story:
+                'This is the view of the headline component with H2 classification and format.',
+        },
+    },
+}
+H2.args = {
+    classification: EHeadlines.H2,
+    format: EHeadlines.H2,
+    title: 'Headline',
+}
+
+const H3 = Default.bind({})
+H3.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story:
+                'This is the view of the headline component with H3 classification and format.',
+        },
+    },
+}
+H3.args = {
+    classification: EHeadlines.H3,
+    format: EHeadlines.H3,
+    title: 'Headline',
+}
+
+const H4 = Default.bind({})
+H4.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story:
+                'This is the view of the headline component with H4 classification and format.',
+        },
+    },
+}
+H4.args = {
+    classification: EHeadlines.H4,
+    format: EHeadlines.H4,
+    title: 'Headline',
+}
+
+const H5 = Default.bind({})
+H5.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story:
+                'This is the view of the headline component with H5 classification and format.',
+        },
+    },
+}
+H5.args = {
+    classification: EHeadlines.H5,
+    format: EHeadlines.H5,
+    title: 'Headline',
+}
+
+const H6 = Default.bind({})
+H6.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story:
+                'This is the view of the headline component with H6 classification and format.',
+        },
+    },
+}
+H6.args = {
+    classification: EHeadlines.H6,
+    format: EHeadlines.H6,
+    title: 'Headline',
+}
+
+const information = `
+### IPropsHeadline
+See above for the full interface definition.
+
+### Events
+The following events will be emitted from the component:
+- \`none\`
+
+### Further Information
+- \`none\`
+`
+const Information: StoryFn = () => ({})
+Information.decorators = [
+    () => ({
+        data() {
+            return {
+                showInfo: false,
+            }
+        },
+        template: `
+          <div>
+            <button @click="showInfo = !showInfo">Toggle Info</button>
+            <pre v-if="showInfo" style="font-size: 0.5rem">${information}</pre>
+          </div>
+        `,
+    }),
+]
+Information.parameters = {
+    docs: {
+        canvas: {
+            sourceState: 'hidden',
+        },
+        description: {
+            story: information,
+        },
+    },
+}
+
+export default meta
+export {Default, H2, H3, H4, H5, H6, Information}
